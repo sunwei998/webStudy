f32c1902c2342bb321d0a24c3c398d23be8ed53b
cr7 <lilaobanmsl88@gail.com>
2022-06-11 16:43:36 +0800

    task

---
+++ Stage2/03.Node/nodePackageManger/sever/app.js
@@ -0,0 +1,92 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const app = new Koa()
+const router = new Router()
+
+const views = require('koa-views')
+const co = require('co')
+const convert = require('koa-convert')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const debug = require('debug')('koa2:server')
+const path = require('path')
+
+const config = require('./config')
+const routes = require('./routes')
+
+const port = process.env.PORT || config.port
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser())
+  .use(json())
+  .use(logger())
+  .use(require('koa-static')(__dirname + '/public'))
+  .use(views(path.join(__dirname, '/views'), {
+    options: { settings: { views: path.join(__dirname, 'views') } },
+    map: { 'njk': 'nunjucks' },
+    extension: 'njk'
+  }))
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - $ms`)
+})
+
+// router.get('/', async (ctx, next) => {
+//   // ctx.body = 'Hello World'
+//   ctx.state = {
+//     title: 'Koa2'
+//   }
+//   await ctx.render('index', ctx.state)
+// })
+
+router.get('/enroll', (ctx) => {
+  const { username, password_a, password_b } = ctx.request.query;
+  let flag_1 = false;
+  let flag_2 = false;
+  let flag_3 = false;
+  const len_1 = username;
+  const uPattern_a = /^[a-zA-Z0-9_-]{4,16}$/
+  flag_1 = uPattern_a.test(len_1);
+  console.log('flag_1: ', flag_1);
+  const len_2 = password_a;
+  const uPattern_b = /^[a-zA-Z0-9_]{8,15}$/
+  flag_2 = uPattern_b.test(len_2);
+  console.log('flag_2: ', flag_2);
+  const len_3 = password_b;
+  flag_3 = len_3 === len_2 ? true : flase;
+  console.log('flag_3: ', flag_3);
+  if (!flag_1) {
+    ctx.response.body = '用户名不符合要求';
+  }
+  if (!flag_2) {
+    ctx.response.body = '密码输入格式错误';
+  }
+  if (!flag_3) {
+    ctx.response.body = '密码输入不一致';
+  }
+  if (flag_1 && flag_2 && flag_3) {
+    ctx.response.body = '注册成功';
+  }
+})
+
+
+routes(router)
+app.on('error', function (err, ctx) {
+  console.log(err);
+  logger.error('server error', err, ctx)
+})
+
+module.exports = app.listen(config.port, () => {
+  console.log(`Listening on http://localhost:${config.port}`)
+})
