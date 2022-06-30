fa504da3ecf521aea5742e54e3039a8e05a59d3c
cr7 <lilaobanmsl88@gail.com>
2022-07-01 00:21:52 +0800

    6.30

---
+++ Stage2/06.Vue.js/day04/my_demo/todolist/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Todolist></Todolist>
+  </div>
+</template>
+
+<script>
+import Todolist from './components/Todolist.vue'
+export default {
+  components: {
+    Todolist
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  /* color: #2c3e50; */
+  /* margin-top: 60px; */
+}
+</style>
---
+++ Stage2/06.Vue.js/day04/my_demo/todolist/src/components/list-item.vue
@@ -0,0 +1,81 @@
+<template>
+    <div
+      :class="['task-item', { status: item.checked }]"
+      @click="changeStatus()"
+    >
+      <div class="flag_img" v-show="item.checked">
+        <img src="../imgs/flag.png" alt="" width="32px" />
+      </div>
+      <div class="task-name">
+        {{ item.name }}
+      </div>
+      <div class="cancel-button" @click.stop="cancelTask">
+        <img src="../imgs/cancel.png" alt="" width="24px" />
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        item:{
+            type:Object
+        },
+        index:{
+            type:Number
+        }
+    },
+    methods:{
+        changeStatus(){
+            this.$emit('changeStatus',this.item)
+        },
+        cancelTask(){
+            this.$emit('cancelTask',this.index)
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+.task-item{
+    display:flex;
+    width:100%;
+    height: 42px;
+    /* justify-content: center; */
+    align-items: center;
+    padding: 10px 10px;
+    position: relative;
+    box-sizing: border-box;
+}
+.flag_img{
+    width: 24px;
+    height: 24px;
+    overflow: hidden;
+    border-radius: 50%;
+}
+.task-name{
+    font-size: 14px;
+    margin-left: 18px;
+}
+.cancel-button{
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    overflow: hidden;
+    position: absolute;
+    right: 15px;
+}
+
+.task-item:nth-of-type(odd){
+    background-color:bisque
+}
+
+.task-item:nth-of-type(even){
+    background-color:rgb(223, 239, 239);
+}
+.container .status{
+    background-color: pink;
+    text-decoration: line-through;
+}
+</style>
